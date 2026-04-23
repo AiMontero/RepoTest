25da54aa995d09e7393f7a1201053ebac1689b1e
Alfredo <alfred.mt@hotmail.com>
2026-04-23 01:06:01 +0000

    parte

--- main.tf
+++ main.tf
@@ -11,9 +11,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Obtiene la VPC por defecto de la región
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Crea el Security Group solicitado en el ejercicio
+resource "aws_security_group" "test_terraform_sg" {
+  name        = "test-terraform-sg"
+  description = "Permite acceso SSH y salida libre"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Regla de entrada: SSH por puerto 22/TCP
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Regla de salida: cualquier destino
+  egress {
+    description = "Todo el trafico saliente"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "test-terraform-sg"
+  }
+}
+
+# Instancia EC2 asociada al SG creado
 resource "aws_instance" "mi_ec2" {
-  ami           = "ami-0e86e20dae9224db8"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c101f26f147fa7fd"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.test_terraform_sg.id]
 
   tags = {
     Name = "test-terraform-ec2"
